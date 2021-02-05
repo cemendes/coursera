b4c24a8af65bc2088a9c0499c90c1ecf5ba7d592
Eduardo Oliveira <eduardo.mendes@gmail.com>
2021-02-05 06:12:17 -0600

    ..

--- find_error.py
+++ find_error.py
@@ -2,17 +2,21 @@ import sys
 import os
 import re
 
+error = input("What is the error? ")
+returned_errors = []
+
 def error_search(log_file):
     pattern = r'(CRON).*\s(ERROR: Failed to start)'
-    returned_errors = {}
+
 
     with open(log_file) as file:
         for line in file:
             result = re.search(pattern, line)
             if result == None:
                 continue
-            returned_errors = {line}
+            returned_errors.append(line)
     return returned_errors
 
 
-print(error_search("/mnt/c/Users/eoliveira/Documents/projects/coursera/fishy.log"))
+#print(error_search("/mnt/c/Users/eoliveira/Documents/projects/coursera/fishy.log"))
+print(error_search("/Users/eoliveira/Documents/projects/coursera-1/fishy.log"))
